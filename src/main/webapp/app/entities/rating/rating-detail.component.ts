4e1556d07c1db62935df0552e616145a5672b665
Adrian Marin <adrianmarinportillo@gmail.com>
2019-05-01 22:22:06 +0200

    Rating details working

--- src/main/webapp/app/entities/rating/rating-detail.component.ts
+++ src/main/webapp/app/entities/rating/rating-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IRating } from 'app/shared/model/rating.model';
+import { Account, AccountService } from 'app/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { JhiAlertService } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-rating-detail',
@@ -9,15 +12,37 @@ import { IRating } from 'app/shared/model/rating.model';
 })
 export class RatingDetailComponent implements OnInit {
     rating: IRating;
+    account: Account;
 
-    constructor(protected activatedRoute: ActivatedRoute) {}
+    constructor(
+        protected activatedRoute: ActivatedRoute,
+        private accountService: AccountService,
+        protected jhiAlertService: JhiAlertService
+    ) {}
 
     ngOnInit() {
+        this.getUser();
         this.activatedRoute.data.subscribe(({ rating }) => {
             this.rating = rating;
         });
+        console.log(this.rating.belongsToPoints);
     }
-
+    getUser() {
+        this.accountService.identifyO().subscribe(
+            response => {
+                const account = response.body;
+                if (account) {
+                    this.account = account;
+                } else {
+                    this.account = account;
+                }
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
     previousState() {
         window.history.back();
     }
